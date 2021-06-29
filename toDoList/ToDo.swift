565401ed49327d180dd88793b41ab84f63d56120
Autumn Halfpenny <autumnhalfpenny@yahoo.com>
2021-06-28 19:49:51 -0500

    Iteration 0 of To-Do List

---
+++ toDoList/ToDo.swift
@@ -0,0 +1,25 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Autumn Halfpenny on 6/28/21.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
+
+/*
+ var newTask = ToDo(taskName : "")
+    
+    init(taskName : String){
+        name = taskName
+    }
+    func writeCode() {
+        print("Task: \(name)")
+    }
+}
+*/
